6030e1fc7b0ebd549342a7f4b3f8ea51b536b97f
Luís Gomes <lmagomes@gmail.com>
2016-05-09 11:52:37 +0100

    Changed paths, to comply with new install.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -13,20 +13,17 @@ export BANANABACKUPID=0B6jHaMqrb3iYal9BRHBUdHZNNkk
 
 # backup files
 crontab -l >  $BACKUPFOLDER/crontab
-cat /etc/fstab > $BACKUPFOLDER/fstab
+cp /etc/fstab $BACKUPFOLDER/etc/fstab
 cat /home/lgomes/.config/me.ini | gpg --batch --yes -r lgomes -e -o $BACKUPFOLDER/me.ini.gpg
 cat /home/lgomes/.config/postgrespw | gpg --batch --yes -r lgomes -e -o $BACKUPFOLDER/postgrespw.gpg
-/home/lgomes/projects/python/penv/bin/pip freeze > $BACKUPFOLDER/requirements.txt
+/home/lgomes/projects/penv/bin/pip freeze > $BACKUPFOLDER/requirements.txt
 
 # dump database
 pg_dump -C -h neko | gpg --batch --yes -r lgomes -e -o $BACKUPFOLDER/database.sql.gpg
-
 
 
 # sync data with google drive
 gdrive sync upload --no-progress $BACKUPFOLDER $BACKUPID
 gdrive sync upload --no-progress $BANANABACKUPFOLDER $BANANABACKUPID
 
-
-tar czf /mnt/data/notebooks.tar.gz /home/lgomes/projects/python/notebooks
 tar czf /mnt/data/misc.tar.gz /home/lgomes/projects/misc
